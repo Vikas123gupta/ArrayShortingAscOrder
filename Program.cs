0b113cdff191c2ab377a760c5a85c8cb1978a743
Vikas <121214669+Vikas123gupta@users.noreply.github.com>
2026-08-12 13:09:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /// <summary>
+        /// This program is use to Short Your array in asc order 
+        /// </summary>
+        /// <param name="args"></param>
+        
+        static void Main(string[] args)
+        {
+            int[] IntegerArray = { 2, 4, 3, 1, 6, 5 };
+            int IntegerArrayLength = IntegerArray.Length;
+            for (int i = 0; i < IntegerArrayLength - 1; i++)
+            {
+                for (int j = 0; j < (IntegerArrayLength - 1) - i; j++)
+                {
+                    if (IntegerArray[j] > IntegerArray[j + 1])
+                    {
+                       int greatervalue = IntegerArray[j];
+                       IntegerArray[j] = IntegerArray[j + 1];
+                       IntegerArray[j + 1] = greatervalue;
+                    }
+                }
+            }
+            for (int i = 0; i < IntegerArrayLength; i++)
+            {
+                Console.WriteLine(IntegerArray[i]);
+            }
+        }
+    }
+}
